1924ce18ca8a812ac76e033ed4ffdf3bd884e97c
thetarkus <mike@mikemob.com>
2020-02-22 20:54:26 -0500

    Add carousel functionality

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,9 +1,31 @@
-const Carousel = {
-  create: ($el) => {
-    this.$el = $el;
-  },
-};
+function Carousel($el) {
+  const self = this;
+  let interval = undefined;
+
+  self.relativeChange = (value) => {
+    const $images = Array.from($el.querySelectorAll('img'));
+    const activeIndex = $images.findIndex(($el) => $el.classList.contains('active'));
+    let nextIndex = activeIndex + value;
+
+    // if higher than amount of images reset to zero
+    if (nextIndex >= $images.length)
+      nextIndex = 0;
+
+    // if negative set to last image
+    else if (nextIndex < 0)
+      nextIndex = $images.length - 1;
+    
+    $images[activeIndex].classList.remove('active');
+    $images[nextIndex].classList.add('active');
+  };
+
+  self.next = () => self.relativeChange(1);
+  self.start = (ms) => interval = setInterval(self.next, ms);
+
+  return self;
+}
 
 document.addEventListener('DOMContentLoaded', function(event) { 
-  Carousel.create(document.querySelector('.carousel'));
+  const carousel = new Carousel(document.querySelector('.carousel'));
+  carousel.start(10000);
 });
